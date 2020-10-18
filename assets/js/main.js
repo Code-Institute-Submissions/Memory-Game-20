e9a00c324fff6e77642a7b220ae81014081eeb06
Lyubomila Petkova <Lyubomila.Petkova@KPMG.co.uk>
2020-10-18 12:32:09 +0100

    Fixed scroll revealing effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,26 @@
 /*====== RELLAX =====*/
 var ellax = new Rellax('.rellax');
 
-/*====== AOS =====*/
-$(function (){
-    AOS.init({
-        easing: 'ease',
-        duration: 1000,
-    });
+/*====== ANIMATE GSAP ======*/
+
+
+/*Header*/
+gsap.from('.header_title', {opacity:0, duration: 3, delay: 1.3, y: 35, ease:'expo.out'});
+gsap.from('.header_button', {opacity:0, duration: 3, delay: 1.1 , y: 35, ease:'expo.out'});
+
+/* Cards page - Modals */
+
+gsap.from('.name-input', {opacity:0, duration: 3, delay: 1 , y: 35, ease:'expo.out'});
+gsap.from('.popup', {opacity:0, duration: 3, delay: 1, y: 35, ease:'expo.out'});
+gsap.from('.timeout-modal', {opacity:0, duration: 3, delay: 1, y: 35, ease:'expo.out'});
+
+/*====== SCROLL REVEAL SECTION ======*/
+const sr = ScrollReveal({
+    duration: 2500,
+    reset: true
 });
+
+sr.reveal('.rules_title',{distance: '70px'}); 
+sr.reveal('.game-rules',{distance: '70px'}); 
+sr.reveal('.lets-play',{distance: '70px'}); 
+sr.reveal('.lets-play-btn',{distance: '70px'}); 
